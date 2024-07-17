ab963f3ae97b2966f94cbae17557667aa61ab0fc
Abubakar kehinde <abubakarkehinde88@gmail.com>
2024-07-17 16:06:48 +0100

    working on mongodb

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 const express = require("express");
+//Step 1: We are going to require mongoose after it has been install
+const mongoose = require("mongoose");
 const app = express();
+mongoose.set('strictQuery', false);
 
 // Adding middleware which will be able to passed data through the body
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // JSON Destructure
 const customers = [
@@ -41,8 +44,21 @@ app.post('/', (req, res) => {  //Post is going to be used to add data.
     res.send('This is a post request');
 })
 
-app.listen(PORT, () => {
-  console.log("App listening on port " + PORT);
-});
+// Step 2: To use Mongoose we're going to define an async function
+const start = async() => {
+    try{
+        await mongoose.connect('mongodb+srv://KEHINDE01:Harbiodun01@cluster0.r1nwvs8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');   // Here is where to pass the connection string
+
+        app.listen(PORT, () => {
+            console.log("App listening on port " + PORT);
+        });
+    } catch(err) {
+        console.log(err.message)
+        
+    }
+};
+start();  // This is called invoke
+
+
 
 
