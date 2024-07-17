d24f0aa2eeab5c4c296eb4048930a25b50757512
Abubakar kehinde <abubakarkehinde88@gmail.com>
2024-07-17 14:24:13 +0100

    json destructure

--- src/app.js
+++ src/app.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const app = express();
+
+// Adding middleware which will be able to passed data through the body
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
 const PORT = 5000;
 
 // JSON Destructure
@@ -26,6 +31,11 @@ app.get('/', (req, res) => {
 app.get('/api/customers', (req, res) => {   // Get is to retrieve data
     res.send({"customers": customers});
 })
+
+app.post('/api/customers', (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
+});
 
 app.post('/', (req, res) => {  //Post is going to be used to add data.
     res.send('This is a post request');
